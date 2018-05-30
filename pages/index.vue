497a559bdab4e7b231a800380c79248ea346e2b7
uucyan <uuunouucyan@gmail.com>
2018-05-30 22:17:17 +0900

    Pug, CoffeeScript, Sass を使えるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,31 @@
-<template>
-	<section class="container">
-		<div>
-			<img width="256" src="~/assets/img/logo.png">
-			<h1>lgtm-cabinet</h1>
-			<h2>Nuxt + Electron</h2>
-			<a href="https://nuxtjs.org/" target="_blank" class="btn btn-primary">Documentation</a>
-			<a href="https://github.com/nuxt/nuxt.js" target="_blank" class="btn btn-primary">GitHub</a>
-			<a href="https://electronjs.org/" target="_blank" class="btn btn-secondary">Electron</a>
-			<a href="https://github.com/electron-userland/electron-builder" target="_blank" class="btn btn-secondary">Electron Builder</a>
-		</div>
-	</section>
+<template lang="pug">
+section.container
+  div
+    img(width='256', src='~/assets/img/logo.png')
+    h1 lgtm-cabinet
+    h2 Nuxt + Electron
+    p {{ helloWorld }}
+    a.btn.btn-primary(href='https://nuxtjs.org/', target='_blank') Documentation
+    a.btn.btn-primary(href='https://github.com/nuxt/nuxt.js', target='_blank') GitHub
+    a.btn.btn-secondary(href='https://electronjs.org/', target='_blank') Electron
+    a.btn.btn-secondary(href='https://github.com/electron-userland/electron-builder', target='_blank') Electron Builder
 </template>
 
-<script>
-export default {
-}
+<script lang="coffee">
+export default
+  name: 'index'
+  data: ->
+    helloWorld: 'はろーわーるど！'
 </script>
 
-<style scoped>
-.container {
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
-.btn {
-	margin: 0 8px;
-}
+<style scoped lang="sass">
+.container
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+
+.btn
+  margin: 0 8px
 </style>
