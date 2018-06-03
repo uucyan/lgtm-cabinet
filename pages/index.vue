7dba98906d4ba34285ba9c6361e3d0519fd23461
uucyan <uuunouucyan@gmail.com>
2018-06-04 02:31:30 +0900

    Elementの導入とサイドバーの実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,21 @@
 <template lang="pug">
-section.container
-  div
-    img(width='256', src='~/assets/img/logo.png')
-    h1 lgtm-cabinet
-    h2 Nuxt + Electron
-    p {{ helloWorld }}
-    a.btn.btn-primary(href='https://nuxtjs.org/', target='_blank') Documentation
-    a.btn.btn-primary(href='https://github.com/nuxt/nuxt.js', target='_blank') GitHub
-    a.btn.btn-secondary(href='https://electronjs.org/', target='_blank') Electron
-    a.btn.btn-secondary(href='https://github.com/electron-userland/electron-builder', target='_blank') Electron Builder
+article
+  sidebar
 </template>
 
 <script lang="coffee">
+import Sidebar from '~/components/sidebar.vue'
 export default
   name: 'index'
   data: ->
     helloWorld: 'はろーわーるど！'
+  components:
+    'sidebar': Sidebar
 </script>
 
 <style scoped lang="sass">
-.container
+article
   min-height: 100vh
+  min-width: 100vh
   display: flex
-  justify-content: center
-  align-items: center
-  text-align: center
-
-.btn
-  margin: 0 8px
 </style>
