0d6c8f552f6a9977c5ef541927243596b44f9331
uucyan <uuunouucyan@gmail.com>
2018-07-29 14:46:15 +0900

    Vueのスタイルガイドに合わせた

--- components/FolderManagementDialog.vue
+++ components/FolderManagementDialog.vue
@@ -1,16 +1,28 @@
 <template lang="pug">
-el-dialog(title="フォルダ管理", :visible.sync="folderManagementDialogVisible", width="50%", :before-close="close")
+el-dialog(
+  title="フォルダ管理",
+  :visible.sync="folderManagementDialogVisible",
+  width="50%",
+  :before-close="close"
+)
   span ここに画像フォルダの一覧を表示する。
   br
   span 画像フォルダの追加や削除はこのダイアログから行う。
-  span(slot="footer", class="dialog-footer")
+  span(
+    slot="footer",
+    class="dialog-footer"
+  )
     el-button(@click="close") 閉じる
-    el-button(type="primary", @click="close") 確定
+    el-button(
+      type="primary",
+      @click="close"
+    ) 確定
 </template>
 
 <script lang="coffee">
 export default
-  props: ['folderManagementDialogVisible']
+  props:
+    folderManagementDialogVisible: Boolean
   methods:
     close: -> this.$emit('close')
 </script>
