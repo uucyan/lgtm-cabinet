d53ad8b60574e61ce9f3c8a0fa53a4f3f823d152
uucyan <uuunouucyan@gmail.com>
2019-01-02 13:24:36 +0900

    とりあえずロゴとタイトルだけのトップ画面を実装

--- components/TheMainView.vue
+++ components/TheMainView.vue
@@ -3,22 +3,24 @@ el-container
   main-view-folder-management(v-show="selectedMenu == 'folderManagement'")
   main-view-config(v-show="selectedMenu == 'config'")
   main-view-image-list(v-show="selectedMenu == 'folder'")
-  el-main.wood-grain-white.z-index-0(v-show="selectedMenu == ''")
+  main-view-top(v-show="selectedMenu == ''")
 </template>
 
 <script lang="coffee">
-import MainViewImageList from '~/components/main/MainViewImageList.vue'
+import MainViewTop from '~/components/main/MainViewTop.vue'
 import MainViewFolderManagement from '~/components/main/MainViewFolderManagement.vue'
 import MainViewConfig from '~/components/main/MainViewConfig.vue'
+import MainViewImageList from '~/components/main/MainViewImageList.vue'
 
 export default
   name: 'MainView'
   computed:
     selectedMenu: -> @$store.state.state.selectedMenu
   components:
-    'main-view-image-list': MainViewImageList
+    'main-view-top': MainViewTop
     'main-view-folder-management': MainViewFolderManagement
     'main-view-config': MainViewConfig
+    'main-view-image-list': MainViewImageList
 </script>
 
 <style lang="sass" scoped>
---
+++ components/main/MainViewTop.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+el-container.main
+  el-header.wood-grain-dark-brown.z-index-1(height='85px' style='padding: 20px 20px 0px 20px; filter: drop-shadow(10px 10px 10px rgba(0,0,0,0.5));')
+    div.header-element-left
+      p.header-title.overflow-x-auto
+  el-main.wood-grain-white.z-index-0.main-top
+    img(
+      src="~assets/img/logo.png"
+      class="logo"
+    )
+    div.title
+      p LGTM Cabinet
+</template>
+
+<script lang="coffee">
+import handleResizeMixin from "~/components/main/handleResizeMixin.coffee"
+
+export default
+  name: 'MainViewFolderTop'
+  mixins: [ handleResizeMixin ]
+</script>
+
+<style lang="sass" scoped>
+.main-top
+  text-align: center
+
+.logo
+  width: 256px
+  height: 256px
+  filter: drop-shadow(10px 10px 10px rgba(0,0,0,0.5))
+
+.title
+  font-family: 'Fredoka One', sans-serif
+  font-size: 80px
+  color: $color-white
+  filter: drop-shadow(10px 10px 10px rgba(0,0,0,0.5))
+</style>
